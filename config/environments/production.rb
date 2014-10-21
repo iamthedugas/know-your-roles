c6c4bbd4b3d30f1babc0ac1fb83a95c440c492c3
iamthedugas <iamthedugas@gmail.com>
2014-10-20 20:12:18 -0400

    production.rb end fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,8 +77,9 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-end
 
   # Required for Heroku
   # Note to set this to the actual host
   config.action_mailer.default_url_options = { :host => 'dugaspinteresting.herokuapp.com'}
+
+end
